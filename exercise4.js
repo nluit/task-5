60429891bd0ffa88b30dd0f2749df278dc1f2fce
nluit <nluit99@gmail.com>
2019-11-21 23:05:52 +0700

    sua loi

--- exercise4.js
+++ exercise4.js
@@ -5,6 +5,7 @@ window.addEventListener('load', function() {
             var filename = document.getElementById('filename').value;
             return filename.substring(filename.lastIndexOf('.') + 1);
         }
+
         alert('The Extention of filename :' + getExtention());
         console.log('The Extention of filename : ' + getExtention());
     })
